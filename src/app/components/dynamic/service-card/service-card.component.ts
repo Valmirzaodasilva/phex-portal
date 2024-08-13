34137d0df4e214e136764d89cd68ea24e1a4ee6d
Josiel Faria <developerjosiel@gmail.com>
2024-08-13 11:33:53 -0300

    fix service card

--- src/app/components/dynamic/service-card/service-card.component.ts
+++ src/app/components/dynamic/service-card/service-card.component.ts
@@ -1,28 +1,37 @@
-import { Component, Input } from '@angular/core';
-import { DataComponentModel } from 'src/app/shared/models/dynamic-page-models/dynamic-page.model';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ImagesService } from 'src/app/shared/services/images.service';
 
-class DataServiceCardModel {
+class ServiceItemModel {
   icon: string;
   title: string;
   text: string;
   textColor: string;
   nameButton: string;
+  url: string;
   iconSize: number;
-  screenSize: number;
-  images: string[];
+  images: Array<string>;
 }
-
-/**
- * @DOCUMENTATION
- * Requisitos: title, text, icon, buttonText, buttonLink, sizeIcon, sizeMobile, sizeTablet, sizeDesktop
- * Obs: atualize o ComponentTypeEnum
- */
 
 @Component({
   selector: 'app-service-card',
   templateUrl: './service-card.component.html',
   styleUrls: ['./service-card.component.scss'],
 })
-export class ServiceCardComponent {
-  @Input() data: DataComponentModel;
+export class ServiceCardComponent implements OnChanges {
+  @Input() data: ServiceItemModel;
+
+  dataDisplay: Array<ServiceItemModel>;
+
+  constructor(private imagesService: ImagesService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['data']?.currentValue) {
+      this.dataDisplay = changes['data'].currentValue;
+    }
+  }
+
+  getImageUrl(image: Array<string>): string {
+    let firstImage = image[0];
+    return this.imagesService.getImages(firstImage);
+  }
 }
