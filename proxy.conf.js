d301f0cbfed01d8b3e30c65d8e40eecdf34af631
Josiel Faria <developerjosiel@gmail.com>
2025-04-02 16:08:02 -0300

    update api

--- proxy.conf.js
+++ proxy.conf.js
@@ -2,7 +2,7 @@ const proxy = [
   {
     context: "/api",
     // target: "http://localhost:3030",
-    target: "https://phex-api.onrender.com",
+    target: "	http://srv779085.hstgr.cloud:3030",
     pathRewrite: { "^/api": "" },
     secure: false,
     changeOrigin: true
