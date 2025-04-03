739de3b870d2fae5268d002d4deb770f5d0d75da
Josiel Faria <developerjosiel@gmail.com>
2025-04-03 11:35:19 -0300

    fix

--- proxy.conf.js
+++ proxy.conf.js
@@ -2,7 +2,7 @@ const proxy = [
   {
     context: "/api",
     // target: "http://localhost:3030",
-    target: "	http://srv779085.hstgr.cloud:3030",
+    target: "https://srv779085.hstgr.cloud:3000",
     pathRewrite: { "^/api": "" },
     secure: false,
     changeOrigin: true
