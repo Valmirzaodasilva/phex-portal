f7856d6fbd6015c92f638fd808bb2df0095ebf8b
Josiel Faria <developerjosiel@gmail.com>
2025-07-31 12:40:59 -0300

    update cors

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,8 +1,8 @@
 const proxy = [
   {
     context: "/api",
-    target: "http://localhost:3030",
-    // target: "https://srv779085.hstgr.cloud:3000", // CONFI PARA RODAR LOCALMENTE
+    // target: "http://localhost:3030", // LOCAL
+    target: "https://srv779085.hstgr.cloud",
     pathRewrite: { "^/api": "" },
     secure: false,
     changeOrigin: true
